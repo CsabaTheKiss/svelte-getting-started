de396d678a48cf0fc589348911a469064f2c4ffb
Csaba Kiss <cocmangamail@gmail.com>
2020-04-09 13:29:57 +0200

    Adding some styles and custom button component

--- src/library/Library.svelte
+++ src/library/Library.svelte
@@ -1,4 +1,33 @@
-<script></script>
-<style></style>
+<script>
+  import Button from '../common/Button.svelte';
 
-<div>Yoyo library</div>
+</script>
+<style>
+  header {
+    margin: var(--spacingMedium) 0 var(--spacingLarge) 0;
+    text-transform: uppercase;
+  }
+  .preamble {
+    display: block;
+  }
+  h1 {
+    font-size: var(--typeSizeXXLarge);
+    font-weight: var(--typeWeightBold);
+    line-height: var(--typeLineHeightTight);
+  }
+
+  .greeting {
+    font-size: var(--typeSizeSmall);
+  }
+</style>
+
+<header>
+  <span class="preamble">Welcome to the</span>
+  <h1>Library</h1>
+</header>
+
+<p class="greeting">
+  This is a library for the people. Welcome. Read, be inspired, share.
+</p>
+
+<Button>+ Add Book</Button>
